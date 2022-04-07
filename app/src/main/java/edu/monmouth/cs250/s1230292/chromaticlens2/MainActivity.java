e2ee32a9203863d2adc519df8760443542d4813c
Michelle Chis <s1230292@monmouth.edu>
2022-04-07 14:20:33 -0400

    Initial commit

---
+++ app/src/main/java/edu/monmouth/cs250/s1230292/chromaticlens2/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.monmouth.cs250.s1230292.chromaticlens2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
