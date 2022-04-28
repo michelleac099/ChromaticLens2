e256380bd0c5d2143c000e67b872ea1832a1bb8d
angelakliks <Angela.kliks@gmail.com>
2022-04-28 00:54:51 -0400

    filters done

--- app/src/main/java/edu/monmouth/cs250/s1230292/chromaticlens2/ImportPicture.java
+++ app/src/main/java/edu/monmouth/cs250/s1230292/chromaticlens2/ImportPicture.java
@@ -1,5 +1,6 @@
 package edu.monmouth.cs250.s1230292.chromaticlens2;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -19,27 +20,50 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-//import com.google.android.material.navigation.NavigationBarView;
 
 public class ImportPicture extends Activity {
+
+    Button saveImage;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.import_screen);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        saveImage = findViewById(R.id.btn_save);
+
+        //IMPORT BTN
         Button gallery = findViewById(R.id.gallery);
+
+        ActivityCompat.requestPermissions(ImportPicture.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(ImportPicture.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 3);
+
+            }
+        });
+
+        saveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToGallery();
             }
         });
     }
@@ -55,6 +79,7 @@ public class ImportPicture extends Activity {
 
         }
     }
+
 
     // monochromatic button click, turns color black and white
     public void onMonoClick(View view) {
@@ -80,10 +105,11 @@ public class ImportPicture extends Activity {
         ColorMatrix matrix2 = new ColorMatrix();
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix2);
         imageView.setColorFilter(new ColorMatrixColorFilter(new ColorMatrix(new float[]{
-                1, 1, 0, 0, 1,
-                0, 0, 1, 0, 1,
-                0, 1, 1, 0, 0,
-                1, 1, 0, 0, 0
+                (float) 0.950,  (float) 0.050, 0, 0, 0,
+                0,  (float) 0.433,  (float) 0.567, 0, 0,
+                0,  (float) 0.475,  (float) 0.525, 0, 0,
+                0, 0, 0, 1, 0
+
                 // R’ = a*R + b*G + c*B + d*A + e;
                 //  G’ = f*R + g*G + h*B + i*A + j;
                 //  B’ = k*R + l*G + m*B + n*A + o;
@@ -91,7 +117,7 @@ public class ImportPicture extends Activity {
         })));
     }
 
-    //on deutranopia click
+
     // Deuteran = protan + more yellow
     // NO GREEN
     public void onDueClick(View view) {
@@ -99,43 +125,25 @@ public class ImportPicture extends Activity {
         ColorMatrix matrix3 = new ColorMatrix();
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix3);
         imageView.setColorFilter(new ColorMatrixColorFilter(new ColorMatrix(new float[]{
-                1, 1, 0, 0, 1,
-                1, 1, 0, 0, 0,
-                0, 1, 1, 0, 0,
-                1, 0, 0, 1, 0
+                (float)0.625, (float)0.375, 0, 0, 0,
+                (float)0.7, (float)0.3, 0, 0, 0,
+                0, (float)0.3, (float)0.7, 0, 0,
+                0, 0, 0, 1, 0
         })));
     }
 
     // on protanopia click
     // NO RED
     public void onProClick(View view) {
+
         ImageView imageView = findViewById(R.id.imageView);
         ColorMatrix matrix4 = new ColorMatrix();
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix4);
         imageView.setColorFilter(new ColorMatrixColorFilter(new ColorMatrix(new float[]{
-
-                //current
-                1, 1, 0, 0, 1,
-                1, 1, 0, 0, 0,
-                0, 0, 1, 0, 0,
-                1, 0, 0, 0, 1
-
-                //  VERY YELLOW
-//                1, 1, 0, 0, 1,
-//                1, 1, 0, 0, 0,
-//                0, 0, 1, 0, 0,
-//                1, 0, 0, 0, 1
-
-                //right colors but is very white
-//                1, 1, 0, 0, 1,
-//                1, 1, 0, 0, 0,
-//                0, 1, 1, 0, 0,
-//                0, 0, 0, 1, 0
-
-//                [ 1 0 0 0 0   - red vector
-//                0 1 0 0 0   - green vector
-//                0 0 1 0 0   - blue vector
-//                0 0 0 1 0 ] - alpha vector
+                (float) 0.567, (float) 0.433, 0, 0, 0,
+                (float) 0.558, (float) 0.442, 0, 0, 0,
+                0, (float) 0.242, (float) 0.758, 0, 0,
+                0, 0, 0, 1, 0
 
         // R’ = a*R + b*G + c*B + d*A + e;
                 //  G’ = f*R + g*G + h*B + i*A + j;
@@ -144,10 +152,37 @@ public class ImportPicture extends Activity {
         })));
     }
 
+    public void saveToGallery() {
 
-    public void imageToRoll(View view) {
+        ActivityCompat.requestPermissions(ImportPicture.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+
+        FileOutputStream outputStream = null;
+        File file = Environment.getExternalStorageDirectory();
+        File dir = new File(file.getAbsolutePath() + "/MyPics");
+        dir.mkdirs();
+
+        String filename = String.format("%d.png",System.currentTimeMillis());
+        File outFile = new File(dir,filename);
+        try{
+            outputStream = new FileOutputStream(outFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream);
+        try{
+            outputStream.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            outputStream.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
-
-
 }
+
